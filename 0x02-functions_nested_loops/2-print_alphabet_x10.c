03580ae709df6730b3c6f45c09bfadbdcb8fb02b
alxkinyanjui <alexgkinyanjui@gmail.com>
2022-09-13 05:19:03 -0700

    Task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 'x'; b <= 'y'; b++)
+		for (b = '97'; b <= '122'; b++)
 		{
 			_putchar(b);
 		}
