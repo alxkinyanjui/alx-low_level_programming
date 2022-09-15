7ff252331f6ec4746d18cb756f1efef9882552fd
alxkinyanjui <alexgkinyanjui@gmail.com>
2022-09-15 04:26:18 -0700

    Task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,15 +11,13 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-
 		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
-		
 		_putchar(92);
 		if (i < (n - 1))
-			_putchar('\');
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
