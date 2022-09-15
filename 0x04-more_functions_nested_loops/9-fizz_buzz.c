506cd519d9aec62c540bf48db724c8441e9dc006
alxkinyanjui <alexgkinyanjui@gmail.com>
2022-09-15 04:51:15 -0700

    Task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,19 +10,19 @@ int main(void)
 
 	n = 1;
 	printf("%d", n);
-	for (n = 2; n<= 100; n++)
+	for (n = 2; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz ");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz");
+			printf(" Fizz ");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("%d", n);
+			printf(" %d", n);
 		}
 	}
 	printf("\n");
