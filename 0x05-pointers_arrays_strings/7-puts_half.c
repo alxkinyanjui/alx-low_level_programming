bdb22d07555a6bb47f87b872ede81c799bf3e218
alxkinyanjui <alexgkinyanjui@gmail.com>
2022-09-19 10:43:27 -0700

    Task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,12 @@ void puts_half(char *str)
 	index = 0;
 	while (str[index] != '\0')
 		index++;
+
 	half = index / 2;
 
 	if (index % 2 == 1)
+		half++;
+	while (half < index)
 	{
 		_putchar(str[half]);
 		half++;
