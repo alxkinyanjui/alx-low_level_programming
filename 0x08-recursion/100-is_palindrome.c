820f70ab96678f09457f8be073028f5aafce8b3c
alxkinyanjui <alexgkinyanjui@gmail.com>
2022-09-28 05:25:09 -0700

    Task7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,7 +3,7 @@
  * _length - checks the length of a string
  * @s: is the string
  * Return: return the length of the string
- */
+**/
 int _length(char *s)
 {
 	if (*s == '\o')
@@ -17,7 +17,7 @@ int _length(char *s)
  * @lg: is the length of the string
  * @s: the string
  * Return: i if is polindrome or 0 if not
- */
+**/
 int checkp(int i, int lg, char *s)
 {
         if (lg > o)
@@ -42,7 +42,7 @@ int checkp(int i, int lg, char *s)
  * is_palindrome - checks if a string is a palindrome
  * @s: is the string
  * Return: return 1 if the string is a palindrom or 0 otherwise
- */
+**/
 int is_palindrome(char *s)
 {
 	return (checkp(o, _length(s) - 1, s));
