96e24b2089b8126534523c6058111c76a3d231f9
alxkinyanjui <alexgkinyanjui@gmail.com>
2022-09-28 05:13:55 -0700

    Task7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,51 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings.
- * @s1: string 1.
- * @s2: string 2. It can contains a * as a special character.
- * Return: 1 if are identical, 0 if not.
+ * _length - checks the length of a string
+ * @s: is the string
+ * Return: return the length of the string
  */
-int wildcmp(char *s1, char *s2)
+int _length(char *s)
 {
-	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	if (*s == '\0')
 		return (0);
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-	return (0);
+	return (1 + _length(s + 1);
 }
+
+/**
+ * checkp - checks if the string is palindrome
+ * @i: is the index
+ * @lg: is the length of the string
+ * @s: the string
+ * Return: i if is polindrome or 0 if not
+ */
+int checkp(int i, int lg, char *s)
+{
+        if (lg > 0)
+	{
+	        if (s[i] == s[lg])
+		{
+		        return (checkp(i + 1, ls - 1, s));
+		}
+		else if (s[i] != s[lg])
+		{
+			        return (0);
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	return (1);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: is the string
+ * Return: return 1 if the string is a palindrom or 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	return (checkp(0, _length(s) - 1, s));
+}
+
